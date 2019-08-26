a59eece4a854af44b542abd030d707eba0691fac
maddiemcroy <mmcroy@gatech.edu>
2019-08-26 15:45:04 -0400

    added payment info page

---
+++ HeroApp/src/app/payment-info/payment-info.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { PaymentInfoPage } from './payment-info.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PaymentInfoPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [PaymentInfoPage]
+})
+export class PaymentInfoPageModule {}
