87500bf72d2a7b4d7e5ef7e18a990bf105f901f7
ChandlerHall <hallchandlerb@gmail.com>
2019-10-29 23:35:18 -0400

    Created discount location page

---
+++ HeroApp/src/app/pages/discount-location/discount-location.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-discount-location',
+  templateUrl: './discount-location.page.html',
+  styleUrls: ['./discount-location.page.scss'],
+})
+export class DiscountLocationPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
